5e094864d06f296aef5603100a3081242af102ef
Alexander Puckhaber <alexpuckhaber@gmail.com>
2017-06-12 15:59:06 -0400

    more formatting

--- hello-world/hello-world.spec.js
+++ hello-world/hello-world.spec.js
@@ -1,19 +1,19 @@
 var HelloWorld = require('./hello-world');
 
-describe('Hello World', function() {
-  var helloWorld = new HelloWorld();
+describe('Hello World', function () {
+    var helloWorld = new HelloWorld();
 
-  //it('should say hello with no name');
+    //it('should say hello with no name');
 
-  it('says hello world with no name', function() {
-      expect(helloWorld.hello('')).toEqual('Hello, World!');
+    it('says hello world with no name', function () {
+        expect(helloWorld.hello('')).toEqual('Hello, World!');
     });
 
-  it('should say hello to bob', function() {
-      expect(helloWorld.hello('bob')).toEqual('Hello, World!')
-  });
+    it('should say hello to bob', function () {
+        expect(helloWorld.hello('bob')).toEqual('Hello, World!')
+    });
 
-  it('should say hello to sally', function() {
-      expect(helloWorld.hello('sally')).toEqual('Hello, World!')
-  });
+    it('should say hello to sally', function () {
+        expect(helloWorld.hello('sally')).toEqual('Hello, World!')
+    });
 });
--- phone-number/phone-number.spec.js
+++ phone-number/phone-number.spec.js
@@ -11,71 +11,66 @@ var PhoneNumber = require('./phone-number');
 
 describe("", function () {
 
-  //phone number with area code in parentheses and numbers separated by dashes should be cleaned up, leaving a string of just the digits.
-  it("", function () {
+
+  it("phone number with area code in parentheses and numbers separated by dashes should be cleaned up, leaving a string of just the digits.", function () {
     var phone = new PhoneNumber('(123) 456-7890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  //phone number with area code and digits separated by '.' should be cleaned up, leaving a string of only digits
-  it("", function () {
+
+  it("phone number with area code and digits separated by '.' should be cleaned up, leaving a string of only digits", function () {
     var phone = new PhoneNumber('123.456.7890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  //Above test should work with _any_ set of digits, as long as they are in a standard format: 
-  //(first 3 digits separated for area code, then 7 more that may be separated after the third digit)
-  //*Additionally, the first number of the area code and fourth digit overall must be between 2-9
-  it("", function () {
+
+  it("Above test should work with _any_ set of digits, as long as they are in a standard format: \n (first 3 digits separated for area code, then 7 more that may be separated after the third digit) \n*Additionally, the first number of the area code and fourth digit overall must be between 2-9", function () {
     var phone = new PhoneNumber('555.456.7890');
     expect(phone.number()).toEqual('5554567890');
   });
 
-  //phone number with 11 digits should be shortened to 10 by removing the first digit, which should be 1, the country code
-  it("", function () {
+
+  it("phone number with 11 digits should be shortened to 10 by removing the first digit, which should be 1, the country code", function () {
     var phone = new PhoneNumber('11234567890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  //phone number that is invalid (at least 2 spaces next to eachother) should be replaced with 10 zeroes
-  it("", function () {
+  it("phone number that is invalid (at least 2 spaces next to eachother) should be replaced with 10 zeroes", function () {
     var phone = new PhoneNumber('2  1234567890');
     expect(phone.number()).toEqual('0000000000');
   });
 
-  //phone number with 12 or more digits is invalid and should be replaced with 10 zeroes
-  it("", function () {
+
+  it("phone number with 12 or more digits is invalid and should be replaced with 10 zeroes", function () {
     var phone = new PhoneNumber('991234567890');
     expect(phone.number()).toEqual('0000000000');
   });
 
-  //phone number with fewer than 10 digits is invalid and should be replaced with zeroes
-  it("", function () {
+
+  it("phone number with fewer than 10 digits is invalid and should be replaced with zeroes", function () {
     var phone = new PhoneNumber('123456789');
     expect(phone.number()).toEqual('0000000000');
   });
 
-  //PhoneNumber object should identify first 3 digits of a 10 digit phone number as the area code
-  it("", function () {
+
+  it("PhoneNumber object should identify first 3 digits of a 10 digit phone number as the area code", function () {
     var phone = new PhoneNumber('1234567890');
     expect(phone.areaCode()).toEqual('123');
   });
 
-  //Above function should work with all valid phone numbers
-  it("", function () {
+
+  it("Above function should work with all valid phone numbers", function () {
     var phone = new PhoneNumber('5554567890');
     expect(phone.areaCode()).toEqual('555');
   });
 
-  //PhoneNumber object should retain original phone number input string and store it
-  //Function toString() should return a string with 3 digit area code surrounded by parenthesis, followed by a space, the next 3 digits, a '-', and the last 4 digits
-  it("", function () {
+
+  it("PhoneNumber object should retain original phone number input string and store it \n Function toString() should return a string with 3 digit area code surrounded by parenthesis, followed by a space, the next 3 digits, a '-', and the last 4 digits", function () {
     var phone = new PhoneNumber('1234567890');
     expect(phone.toString()).toEqual('(123) 456-7890');
   });
 
-  //above function should work for all valid phone numbers
-  it("", function () {
+  it("above function should work for all valid phone numbers", function () {
     var phone = new PhoneNumber('5554567890');
     expect(phone.toString()).toEqual('(555) 456-7890');
   });
